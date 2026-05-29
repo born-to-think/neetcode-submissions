a149be661bc2106fff4834e34ce79b3d1dc2ad1f
Jatin <jatinjindal.ai@gmail.com>
2026-05-29 11:43:45 +0530

    Clarify logic for updating height index in algorithm
    Added comments explaining the logic behind updating the smaller height index in the max water container algorithm.

--- Data Structures & Algorithms/max-water-container/submission-0.cpp
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -12,6 +12,12 @@ public:
         int j = 0, k= heights.size()-1;
         while(j<k){
             res = max ( res, area(heights,j,k) );
+            //// what is the explaination of why we updated only smaller height index??
+            // https://youtu.be/ZHQg07n_tbg?si=oL6OjYRVxq2vU41r
+            // because suppose smaller height = x;
+            // it is enivitable to get any better answer using x whatever the heights in "WHOLE Array" are....
+            // if x is valueable, it already has contributed in the above instruction and result of x (ie. max possible result x can ever give) is stored in res,
+            // so why preserve x?
             if(heights[j]<heights[k])j++;
             else k--;
         }
